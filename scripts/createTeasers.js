f244c3c1607294cb5361bddd275f317b43a395a3
takanori <takanori0170@gmail.com>
2020-07-02 20:18:44 +0900

    create-teasers

---
+++ scripts/createTeasers.js
@@ -0,0 +1,26 @@
+const nodeHtmlToImage = require('node-html-to-image')
+const fs = require('fs');
+const { resolve } = require('path');
+
+const dir = './_site/teasers'
+
+fs.readdir(dir, { withFileTypes: true }, (err, dirents) => {
+  if (err) throw err;
+  dirents.forEach(({ name }) => {
+    const fileType = name.split('.')[1]
+    if (fileType !== 'html') return
+    fs.readFile(`${dir}/${name}`, 'utf-8', async (err, data) => {
+      if (err) throw err;
+      await nodeHtmlToImage({
+        output: `${dir}/${name.split('.')[0]}.png`,
+        html: data,
+        puppeteerArgs: {
+          defaultViewport: {
+            width: 800,
+            height: 418
+          }
+        }
+      })
+    })
+  })
+});
